bdb61cd662cf3d4103de47c1547e562e8c950fce
RashadGasimzada <rashad.gasimzade.2000@gmail.com>
2022-02-17 22:13:43 +0400

    product page created

---
+++ assets/js/pages/product.js
@@ -0,0 +1,22 @@
+$(function() {
+    $(".minus").click(function(){
+        if ($(".input-box input").val() > 1) {
+            $(".input-box input").val( function(i, oldval) {
+                return --oldval;
+            });
+        }
+
+
+    })
+    $(".plus").click(function(){
+        $(".input-box input").val( function(i, oldval) {
+            return ++oldval;
+        });
+        
+
+
+    })
+
+
+
+})
